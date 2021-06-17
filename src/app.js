3daf607c9357215809455cf510abcee6ddcb1d38
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-17 09:08:02 -0300

    customers CR

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pg from 'pg';
-import joi from 'joi';
+import Joi from 'joi';
 
 const app = express();
 app.use(cors());
@@ -17,16 +17,24 @@ const connection = new Pool({
     database: 'boardcamp'
 })
 
-const categorieSchema = joi.object({
-    name: joi.string().min(1).required()
+const categorieSchema = Joi.object({
+    name: Joi.string().min(1).required()
 })
 
-const gameSchema = joi.object({
-    name: joi.string().min(1).required(),
-    image: joi.string().uri(),
-    stockTotal: joi.number().integer().min(1).required(),
-    categoryId: joi.number().integer().required(),
-    pricePerDay: joi.number().integer().min(1).required()
+const customerSchema = Joi.object({
+    name: Joi.string().min(1).required(),
+    phone: Joi.string().min(10).max(11).regex(/^[0-9]+$/).required(),
+    cpf: Joi.string().length(11).regex(/^[0-9]+$/).required(),
+    birthday: Joi.date()
+})
+//const rentalSchema =
+
+const gameSchema = Joi.object({
+    name: Joi.string().min(1).required(),
+    image: Joi.string().uri(),
+    stockTotal: Joi.number().integer().min(1).required(),
+    categoryId: Joi.number().integer().required(),
+    pricePerDay: Joi.number().integer().min(1).required()
 })
 
 //C -> INSERT INTO categories (name) VALUES (a) - ex. INSERT INTO categories (x,y,z) VALUES (a,b,c)
@@ -101,6 +109,34 @@ app.post("/games", async (req, res) => {
 })
 
 //----------------------------------CLIENTS----------------------------------//
+app.get("/customers", async (req, res) => {
+    const query = req.query.cpf ? req.query.cpf + '%' : '%';
+    
+    try {
+        const customers = await connection.query('SELECT * FROM customers WHERE cpf LIKE $1', [query]);
+        res.send(customers.rows);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
+
+app.post("/customers", async (req, res) => {
+    const { name, phone, cpf, birthday } = req.body;
+    if (customerSchema.validate(req.body).error) return res.sendStatus(400);
+
+    try {
+        const repeated = await connection.query('SELECT cpf FROM customers WHERE cpf = $1', [cpf]);
+        if (repeated.rows[0]) return res.sendStatus(409);
+        await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4)',
+            [name, phone, cpf, birthday]);
+        res.sendStatus(201);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
+
 //----------------------------------RENTALS----------------------------------//
 
 app.listen(4000, () => {
